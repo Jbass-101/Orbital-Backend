acb232d5162cb9f9b57ce0c9877d4cbcc4792213
Junior <junior07bj@gmail.com>
2026-02-02 13:12:12 +0200

    Added simulation engine

--- src/main/kotlin/com/jbass/orbital/application/Application.kt
+++ src/main/kotlin/com/jbass/orbital/application/Application.kt
@@ -1,8 +1,10 @@
 package com.jbass.orbital.application
 
+import com.jbass.orbital.data.simulation.SimulationEngine
 import com.jbass.orbital.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
+import org.koin.ktor.ext.get
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -10,6 +12,8 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     configureDI()
+    val engine = get<SimulationEngine>()
+    engine.start()
     configureDiscovery()
     configureSockets()
     configureSerialization()
